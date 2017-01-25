b7d43a1c1e73c08305b1f37d4ad30c67ea8adfc2
taufan99 <atalarikt@gmail.com>
2017-01-25 22:10:13 +0700

    Commit

---
+++ asp_mvc_2/Models/DB/ModelDemo.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'E:\Backup File Flashdisk\Pemrograman Desktop\asp_mvc_2\asp_mvc_2\Models\DB\ModelDemo.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
